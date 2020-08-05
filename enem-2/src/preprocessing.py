e01e7a3de0b53341a6201f6a5e61936d56cc694c
VSDorotea <gylowsboy@gmail.com>
2020-08-05 15:07:18 -0300

    Module 8 slip process method

--- enem-2/src/preprocessing.py
+++ enem-2/src/preprocessing.py
@@ -105,6 +105,48 @@ class Preprocessing:
         if self.data.name_target in self.categoric_features:
             self.categoric_features.remove(self.data.name_target)
 
+    def _process_test(self, df):
+        '''
+        Process train dataframe with transform
+        :param df: Dataframe
+        :return: Transformed dataframe 
+        '''
+        print('Feature Transform in test dataframe')
+        df[self.numeric_features] = self.scaler.transform(
+            df[self.numeric_features])
+        df[self.categoric_features] = self.catb.transform(
+            df[self.categoric_features])
+        for column in df[self.get_feature_names()].columns:
+            # TODO imputar com a média de teste que deve ser armazenada em algum local
+            df[column] = df[column].fillna(df[column].mean())
+        return df[self.get_feature_names()]
+
+    def _process_train(self, df):
+        '''
+        Process train dataframe with fit and transform
+        :param df: Dataframe
+        :return: Fitted and transformed dataframe, and target serie 
+        '''
+        print('Setting Y as target and Removing target from train dataframe')
+        y = df[self.data.name_target].fillna(0)
+        df = df.drop(columns={self.data.name_target})
+
+        self.scaler = StandardScaler()
+        self.catb = ce.CatBoostEncoder(cols=self.categoric_features)
+
+        print('Feature Fit and Transform in train dataframe')
+        df[self.numeric_features] = self.scaler.fit_transform(
+            df[self.numeric_features])
+        df[self.categoric_features] = self.catb.fit_transform(
+            df[self.categoric_features], y=y)
+
+        # TODO implementar testes automatizados para garantir que os dados de x e y continuam correspondentes
+
+        #self.categoric_features = categoric_features
+        #self.numeric_features = numeric_features
+
+        return df[self.get_feature_names()], y
+
     def process(self, is_train_stage=True,
                 missing_values_acceptable=0,
                 unique_values_acceptable=100):
@@ -113,7 +155,7 @@ class Preprocessing:
         :param etapa_treino: Boolean
         :param missing_values_acceptable: Int with minimum missing values acceptable percentage
         :param unique_values_acceptable: Int with maximum unique values acceptable percentage
-        :return: processed Pandas Data Frame
+        :return: processed Pandas Data Frame, and target if train stage
         '''
         df = self.data.read_data(is_train_stage)
 
@@ -123,40 +165,11 @@ class Preprocessing:
             self._preprocess_auto(
                 df, missing_values_acceptable, unique_values_acceptable)
 
+        # TODO deletar quando o PCA estiver funcionando
         if 'TP_ESCOLA_4' in self.categoric_features:
             self.categoric_features.remove('TP_ESCOLA_4')
 
         print(f'Numeric Feature >>>> {self.numeric_features}')
         print(f'Categoric Feature >>>> {self.categoric_features}')
 
-        print('Feature Normalization and Encoding:')
-        if is_train_stage:
-            print('Setting Y as target and Removing target from train dataframe')
-            y = df[self.data.name_target].fillna(0)
-            df = df.drop(columns={self.data.name_target})
-
-            self.scaler = StandardScaler()
-            self.catb = ce.CatBoostEncoder(cols=self.categoric_features)
-
-            print('Feature Fit and Transform in train dataframe')
-            df[self.numeric_features] = self.scaler.fit_transform(
-                df[self.numeric_features])
-            df[self.categoric_features] = self.catb.fit_transform(
-                df[self.categoric_features], y=y)
-
-            # TODO implementar testes automatizados para garantir que os dados de x e y continuam correspondentes
-
-            #self.categoric_features = categoric_features
-            #self.numeric_features = numeric_features
-
-            return df[self.get_feature_names()], y
-        else:
-            print('Feature Transform in test dataframe')
-            df[self.numeric_features] = self.scaler.transform(
-                df[self.numeric_features])
-            df[self.categoric_features] = self.catb.transform(
-                df[self.categoric_features])
-            for column in df[self.get_feature_names()].columns:
-                # TODO imputar com a média de teste que deve ser armazenada em algum local
-                df[column] = df[column].fillna(df[column].mean())
-            return df[self.get_feature_names()]
+        return self._process_train(df) if is_train_stage else self._process_test(df)
